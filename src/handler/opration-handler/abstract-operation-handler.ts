e390cb0d74168eb5fc63a22ac3cf0d3dedb4f16b
zxczoxc125 <zxczoxc125@gmail.com>
2020-08-09 18:43:37 +0900

    🔧 코드 순서 변경

--- src/handler/opration-handler/abstract-operation-handler.ts
+++ src/handler/opration-handler/abstract-operation-handler.ts
@@ -3,10 +3,10 @@ import Request from "../../request/request";
 
 abstract class AbstractOperationHandler extends AbstractHandler {
   handleRequest(request: Request): void {
-    super.handleRequest(request);
-
     const prevEqation: string = request.getEquation();
     request.setEquation(`${prevEqation} ${this.getOperator()} ${this.getOperand().getInput()}`);
+    
+    super.handleRequest(request);
   }
 
   abstract getOperator(): string;
