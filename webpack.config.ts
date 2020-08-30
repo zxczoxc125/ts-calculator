7e2e6a44edea8a8da5ddd18922d4a40d96aba292
zxczoxc125 <zxczoxc125@gmail.com>
2020-08-30 13:17:08 +0900

    🔧 빌드 스크립트 및 관련 설정 수정

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,30 +1,33 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js'
-  },
-  plugins: [new HtmlWebpackPlugin({
-    template: 'index.html'
-  })],
-  module: {
-    rules: [
-      {
-        test: /\.ts?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+module.exports = (env) => {
+  return {
+    entry: './src/index.ts',
+    mode: env.mode,
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'index_bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: 'index.html'
+    })],
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.ts', '.js' ],
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    }
   }
 };
